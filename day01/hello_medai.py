57379cc0cddf77b1d59d8df64db03d4a8fc2c1e9
hgc <hgc20050123@gmail.com>
2026-03-27 12:45:46 +0800

    day02

---
+++ day01/hello_medai.py
@@ -0,0 +1,23 @@
+print("我是临床医学大三学生")
+print("我的方向是:医疗AI/医疗数据/一脚场景工具")
+print("Day 1 started")
+# 这里是我的学习python的第一天,我必须要坚持下去
+print(123)
+# ctrl+z是撤回建  ctrl+/是注释键  ctrl+D是复制这一行到下一行
+''' 123
+123123
+'''
+# 三个引号之间的所有行都不会运行
+print(123)
+print(123)
+print(123)
+print(123)
+print(123)
+print(123)
+print(123)
+print(123)
+print("我要好好学ai")
+print("hhh""xixixi")
+# print("hhh","xixixi")逗号隔开的字符串会被当成两个参数输入print函数，会在输出的时候用空格将他们分开
+print("hhh","xixixi",sep="/")  #sep就是用来间隔多句话
+print("hhh","lululu",end="???")  #end就是用来结束这一行的，默认是换行                    
---
+++ day02/patient_profile.py
@@ -0,0 +1,26 @@
+# 1.变量的作用
+# 计算机的存贮空间
+a = "张三"    
+b = "李四"
+print(a)
+print(b)
+total =  (a+b)#字符串的拼接
+print(total)
+# 2.变量的命名
+# 变量命名的规范
+# 1.只能包含字母、数字和下划线，不能以数字开头
+hg_c = 123
+hgc123 = hg_c
+print(hgc123)
+# 2.不能使用Python的关键字作为变量名
+# 3.变量名应该具有描述性，能够清晰地表达变量的含义
+patient_name = "老王"
+patient_age = 45 #岁
+patient_gender = "女" 
+patient_height = 150 # cm
+patient_weight = 50  # kg
+print("患者姓名:", patient_name)
+print("患者年龄：",patient_age,"岁")
+print("患者性别：",patient_gender)
+print("患者体重：",patient_weight,"kg")
+print("身高:",patient_height,"cm")
